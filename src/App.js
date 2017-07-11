719ffce82f1b4a26c19f6ed75ea4564b9fcc48e2
tuningmind <tuningmind@gmail.com>
2017-07-11 15:03:20 -0600

    Layout set

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ class App extends Component {
           <Stocks />
         </main>
         <footer>
-          <p>getting better putting one foot<span>er</span> in front of another</p>
+          <p>putting one foot<span>er</span> in front of another</p>
         </footer>
       </div>
     );
--- src/components/Stocks.js
+++ src/components/Stocks.js
@@ -2,15 +2,55 @@ import React, { Component } from 'react'
 
 class Stocks extends Component {
 
+  get_max_profit() {
+    const yesterday = [10, 7, 5, 8, 11, 9]
+    const max = Math.max(...yesterday)
+    const min = Math.min(...yesterday)
+    const sell = Number(9) + yesterday.indexOf(max)
+    const buy = Number(9) + yesterday.indexOf(min)
+    return ('The best profit yesterday was purchase of ' 
+            + '$' + min 
+            + ' at '
+            + buy%12 + ':30'
+            + ' and sale of '
+            + '$' + max 
+            + ' at '
+            + sell%12 + ':30')
+  }
+
   render() {
     return (
       <section>
-        <h3>Question</h3>
-        <div id="question"></div>
-        <h3>Answer</h3>
-        <div id="answer"></div>
+        <h2>Question</h2>
+        <div id='question'>
+          <p>Suppose we could access yesterday's stock prices as a list, where:</p>
+          <ul>
+            <li>
+              The indices are the time in minutes past 
+              trade opening time, which was 9:30am local time.
+            </li>
+            <li>
+              The values are the price in dollars of Apple stock 
+              at that time.
+            </li>
+          </ul>
+          <p>
+            So if the stock cost $500 at 10:30am, 
+            stock_prices_yesterday[60] = 500.
+          </p>
+          <p>
+            Write an efficient function that takes 
+            stock_prices_yesterday = [10, 7, 5, 8, 11, 9] and 
+            returns the best profit I could have made from 1 
+            purchase and 1 sale of 1 Apple stock yesterday.
+          </p>
+        </div>
+
+        <h2>Answer</h2>
+        <p id='answer' className='code'>{this.get_max_profit()}</p>
       </section>
     )
   }
 }
-module.export = Stocks
+
+export default Stocks
