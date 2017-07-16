044219f8dfa73205cba4fadeeed6e88e7f7c3781
tuningmind <tuningmind@gmail.com>
2017-07-16 00:26:53 -0600

    Onclick sets state for name of active screen

--- src/App.js
+++ src/App.js
@@ -1,32 +1,46 @@
 import React, { Component } from 'react'
+import { EventEmitter } from 'events'
 import './App.css'
+import Home from './components/Home'
 import Stocks from './components/Stocks'
 
 class App extends Component {
   constructor(props) {
     super(props) 
     this.state = {
+      active: '', 
+      screens: {
+        Home: 'Home', 
+        Stocks: 'Stocks'
+      }
     }
     this.clickhandler = this.clickhandler.bind(this)
   }
 
-  clickhandler() {
-    //when you click on a button, the handler
-    //returns the component whose button you
-    //clicked
+  clickhandler(e) {
+    this.setActivePage(e.target.value)
   }
 
+  setActivePage(page) {
+    this.setState({active: page})
+  }
+
+  componentWillMount() {
+    this.setState({active: 'Home'})
+  }
   render() {
+    const ActiveScreen = this.state.screens[this.state.active]
+
     return (
-      <div className="App">
+      <div className='App'>
         <header>
           <h1>Interview Cake</h1>
         </header>
         <main>
           <aside>
-            <button id="001" onClick={this.clickhandler}>stocks 001</button>
+            <button value='Stocks' onClick={this.clickhandler}>stocks 001</button>
           </aside>
-          <Stocks />
+        <Stocks /> 
         </main>
         <footer>
           <p>putting one foot<span>er</span> in front of another</p>
---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Home extends Component {
+  render() {
+    return(
+      <section>
+        Here is where questions and my answers will appear
+      </section>
+    )
+  }
+}
+
+export default Home
